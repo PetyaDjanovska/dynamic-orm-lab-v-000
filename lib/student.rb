41130ab3d728cd4a331a83a005dbf8c48639ed54
Petya Djanovska <petya.djanovska@gmail.com>
2018-04-10 15:56:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  
+  attr_accessor :id, :name, :grade
   def initialize(options={})
   options.each do |property, value|
     self.send("#{property}=", value)
